5e34822f41b7d841bceff41c2651cbf9f840027b
Hunter Hutchinson <hunter.hutchinson@gmail.com>
2014-08-13 15:55:48 -0400

    first commit

---
+++ test-app/main.js
@@ -0,0 +1,19 @@
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');
+
+console.log('Loading a web page');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is GCed.
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({show: false});
+    mainWindow.loadUrl('http://www.phantomjs.org/');
+    mainWindow.webContents.on('did-finish-load', function() {
+        // window.webContents.send('ping', 'whoooooooh!');
+        console.log("yahtzee!");
+        app.quit();
+    });
+});
+
